c917db823e0d320fa5c84ec3f788205f87de08b8
João <160570832+JoaoMarcelloLopesMontes@users.noreply.github.com>
2025-05-27 20:19:32 -0300

    Update Vetor6.py

--- Lote1.3/Vetor6.py
+++ Lote1.3/Vetor6.py
@@ -9,9 +9,9 @@ vet = [random.randint(1,100) for _ in range(20)]
 print(f"Vetores fora de ordem: {vet}")
 
 for i in range(0,19,1):
-    for j in range(0,20,1):
-         if vet[j] > vet[i]:
-            vet[i], vet[j] = vet[j], vet[i]
+    for j in range(i + 1,20,1):
+         if vet[i] > vet[j]:
+            vet[j], vet[i] = vet[i], vet[j]
 
 
 print(f"Valores ordenados: {vet}")
